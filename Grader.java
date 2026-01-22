f180c60126b6978edec012a172fcfd9e0a2538a5
Elianne07 <ereye400@students.bju.edu>
2026-01-21 22:27:21 -0500

    Add files via upload

--- Grader.java
+++ Grader.java
@@ -9,7 +9,14 @@ public class Grader {
 
             
         for (int i = 0; i < n; i++){
-            int grade = scan.nextInt();
+            int grade;
+
+            // Avoid keep reading empty lines, and stops as soon as the next input line is empty
+            try {
+                grade = scan.nextInt();
+            } catch (NoSuchElementException e) {
+                break; 
+            }
 
             if (grade >= 38){
                 int nextMultiple5 = ((grade / 5) + 1) * 5;
